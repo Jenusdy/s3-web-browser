b7383e0b460fb9edd7c3f64098943d6a1376cb02
Fawcet Jenusdy Makay <jenusdy@gmail.com>
2026-09-02 15:16:58 +0700

    Remove obsolete .env and .env.example files as connections are now managed via SQLite database

--- config.py
+++ config.py
@@ -7,19 +7,4 @@ class Config:  # noqa: D101
     DEBUG = os.getenv("DEBUG", "False")
     SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI", "sqlite:///connections.db")
 
-    AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
-    AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
-    AWS_DEFAULT_REGION = os.getenv("AWS_DEFAULT_REGION", "eu-central-1")
-    AWS_ENDPOINT_URL = os.getenv("AWS_ENDPOINT_URL", None)
-    AWS_DEFAULT_BUCKET = os.getenv("AWS_DEFAULT_BUCKET", None)
-
-    AWS_KWARGS: ClassVar[dict[str, str | None]] = {
-        "aws_access_key_id": AWS_ACCESS_KEY_ID,
-        "aws_secret_access_key": AWS_SECRET_ACCESS_KEY,
-        "region_name": AWS_DEFAULT_REGION,
-    }
-
-    if AWS_ENDPOINT_URL:
-        AWS_KWARGS["endpoint_url"] = AWS_ENDPOINT_URL
-
     PAGE_ITEMS = int(os.getenv("PAGE_ITEMS", "300"))
